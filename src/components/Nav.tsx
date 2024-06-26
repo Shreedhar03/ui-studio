efbba4e75081df505ed6d74d00a8aedfb589e7f0
Shreedhar Deodatta Urawane <urawane03@gmail.com>
2024-06-26 13:19:02 +0530

    recreate Nav and Home

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <>
-      <nav className="flex flex-col items-center justify-center py-4 text-lg font-semibold bg-gradient-to-r from-secondary to-primary to-100% text-white">
+      <nav className="flex items-center justify-between px-12 py-4 text-lg font-semibold bg-gradient-to-r from-primary/20 to-primary/40 to-90%">
         <h1 className="logo text-2xl">UI.Studio.</h1>
         <div className="flex gap-4">
           <Link to="/" className="underline underline-offset-4 font-light">
