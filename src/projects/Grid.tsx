87b4bdc0e15e24a71b3d5ebe5eb0835ab611e559
Shreedhar Deodatta Urawane <urawane03@gmail.com>
2024-06-26 12:48:35 +0530

    Grid: partial

--- src/projects/Grid.tsx
+++ src/projects/Grid.tsx
@@ -14,64 +14,74 @@ const gridEmpty: number[][] = [
 const Grid = () => {
   //   maintain a stack of selected cells
   const [selectedCells, setSelectedCells] = useState<number[][]>(gridEmpty);
-  const [allSelected, setAllSelected] = useState<boolean>(false);
+  const [ones, setOnes] = useState<number>(0);
+  const [selectedOnes, setSelectedOnes] = useState<number>(0);
 
   const handleClick = (i: number, j: number) => {
+    setSelectedOnes((prev) => prev + 1);
     console.log(i, j);
-    const newGrid = [...selectedCells];
-    newGrid[i][j] = 1;
+    const newGrid = selectedCells.map((row, rowIndex) =>
+      row.map((cell, colIndex) => (rowIndex === i && colIndex === j ? 1 : cell))
+    );
+
     setSelectedCells(newGrid);
   };
 
   useEffect(() => {
-    // de select all cells if all cells are selected
-    let flag = true;
-    for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid[i].length; j++) {
-        if (grid[i][j] === 1 && selectedCells[i][j] === 0) {
-          flag = false;
-          break;
-        }
-      }
+    // reset the selected cells when ones are equal to selected ones
+    if (ones === selectedOnes) {
+      setTimeout(() => {
+        setSelectedCells(gridEmpty);
+      }, 1000);
+      setSelectedOnes(0);
     }
+  }, [selectedOnes]);
 
-    setAllSelected(flag);
-
-    console.log(selectedCells);
-  }, [selectedCells]);
+  const handleReset = () => {
+    const newGrid = [...gridEmpty];
+    setSelectedCells(newGrid);
+    setSelectedOnes(0);
+  };
 
   useEffect(() => {
-    if (allSelected) {
-      setSelectedCells(gridEmpty);
-      console.log(`All selected: ${allSelected}`);
-      //   console.log("Selected cells: ", selectedCells);
-      setAllSelected(false);
-    }
-  }, [allSelected]);
+    // calculate no. of 1s in the grid
+    let count = 0;
+    grid.forEach((row) => {
+      row.forEach((cell) => {
+        if (cell === 1) count++;
+      });
+    });
+    setOnes(count);
+    console.log(count);
+  }, []);
 
   return (
-    <div className="flex flex-col gap-3 mt-8">
-      {grid.map((_, i) => {
-        return (
+    <>
+      <div className="flex flex-col gap-3 mt-8">
+        {selectedCells.map((row, i) => (
           <div className="flex gap-3" key={i}>
-            {grid.map((_, j) => {
-              return (
-                <button
-                  disabled={grid[i][j] === 0}
-                  onClick={() => handleClick(i, j)}
-                  className={`w-10 h-10 rounded-md
-                    ${grid[i][j] === 0 && "bg-white border border-gray-200"}
-                    ${
-                      selectedCells[i][j] === 1 ? "bg-green-600" : "bg-gray-200"
-                    }`}
-                  key={j}
-                ></button>
-              );
-            })}
+            {row.map((_, j) => (
+              <button
+                disabled={grid[i][j] === 0 || selectedCells[i][j] === 1}
+                onClick={() => handleClick(i, j)}
+                className={`w-10 h-10 rounded-md transition-all
+                  ${grid[i][j] === 0 && "bg-white border border-gray-200"}
+                  ${
+                    selectedCells[i][j] === 1 ? "bg-green-600" : "bg-gray-200"
+                  }`}
+                key={j}
+              ></button>
+            ))}
           </div>
-        );
-      })}
-    </div>
+        ))}
+      </div>
+      <button
+        onClick={handleReset}
+        className="text-red-700 underline mt-5 text-lg bg-red-700/20 px-3 py-1 rounded-md hover:bg-red-700/30 transition-all"
+      >
+        Reset
+      </button>
+    </>
   );
 };
 
